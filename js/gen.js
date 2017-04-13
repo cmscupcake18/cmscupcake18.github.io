8d8c5291bdf253eb88ad108a002b91f133a2c44a
Kenjin <cmscupcake18@gmail.com>
2017-04-13 18:43:19 +0800

    Add files via upload

---
+++ js/gen.js
@@ -0,0 +1,80 @@
+/// <reference path="js/angular.min.js" />
+
+angular.module("myapp", [])
+                  .controller("mycontroller", function ($scope){
+                  $scope.num ="+63 910 8847547"
+				  $scope.A = "ABOUT ME";
+				  $scope.B = "GENESIS GUZMAN";
+				  $scope.C = "To work with the Company who makes used of full knowledge and skills of the things that I have learned through the years and to gain more confidence for personal and future advancement.";
+				  $scope.D = "As a";
+				  $scope.E = "LEADING THE DEVELOPMENT OPPORTUNITY THROUGH INFORMATION TECHNOLOGY";
+				  $scope.F = "Y4iT RESEARCH SUMMIT 2016";
+				  $scope.G = "INTRODUCTION OF GAME DEVELOPMENT USING  GAMEMAKER:STUDIO";
+				  $scope.H = "JOOMLA: CONTENT MANAGEMENT SYSTEM";
+				  $scope.I = "BASIS CONCEPT OF SOFTWARE TESTING";
+				  $scope.J = "WEB DEVELOPMENT USING WORDPRESS";
+				  $scope.K = "LEARNING ANGULARJS";
+				  $scope.L = "* Y4iT RESEARCH SUMMIT INNOVATION AND BEYOND";
+				  $scope.M = "Speaker:";
+				  $scope.O = "Date:";
+				  $scope.P = "2017";
+				  $scope.Q = "JANUARY";
+				  $scope.R = "FEBUARY";
+				  $scope.S = "MARCH";
+				  $scope.T = "APRIL";
+				  $scope.U = "BASIC KNOWLEDGE";
+				  $scope.V = "SCHOOL BACKGROUND";
+				  $scope.W = "QUOTE";
+				  $scope.X = "SEMINARS ATTENDED";
+				  $scope.Y = "SEMINARS";
+				  $scope.Z = "PHOTO";
+				  $scope.z = "KNOWLEDGE";
+				  $scope.x = "HOME";
+				  $scope.w = "if you FAIL never give up because F.A.I.L means";
+				  $scope.v = "First Attemp In Learning";
+				  $scope.u = "END is not the End, in fact E.N.D mean";
+				  $scope.t = "EFFORT NEVER DIES";
+				  $scope.s = "if you get NO as an answer,Remember N.O means";
+				  $scope.r = "NEXT OPPORTUNITY";
+				  $scope.q = "LETS BE POSITIVE";
+				  $scope.p = "BASKETBALL PLAYER";
+				  $scope.o = "ROADBIKE CYCLIST";
+				  $scope.m = "BSIT STUDENT";
+				  $scope.l = "CATHOLIC";
+				  $scope.k = "*BIG DATA ANALYTICS.";
+				  $scope.j = "*ANDROID MOBILE APPS DEVELOPMENT.";
+				  $scope.i = "*ADOBE MAGIC USING CREATING CLOUD.";
+				  $scope.h = "*ROBOTICS USING ARDUINO";
+				  $scope.g = "CONTACT INFO";
+				  $scope.f = "OBJECTIVE";
+				  $scope.e = "CATHOLIC";
+
+
+                    
+
+
+
+
+				  $scope.nameA = "Mr.RONEL F. RAMOS";
+				  $scope.nameB = "Ms.JULLIANN A. LAPIDEZ";
+				  $scope.nameC = "Mr. LEMWELL P. EVANGELISTA";
+				  $scope.nameD = "Mr.JOHNNY S. RODRIGUEZ";
+				  $scope.nameE = "BS-INFORMATION TECHNOLOGY";
+				  $scope.nameF = "COLEGIO DE SAN GABRIEL ARCANGEL";
+				  $scope.nameG = "";
+				  $scope.nameH = "";
+				  $scope.nameI = "";
+				  $scope.nameJ = "";
+
+				  $scope.gen = "pages/gen.html";
+				  $scope.genn = "pages/genn.html";
+				  $scope.about = "pages/hello.html";
+				  $scope.school = "pages/school.html";
+				  $scope.language = "pages/lang.html";
+				  $scope.db = "pages/base.html";
+				  $scope.others = "pages/other.html";
+				  $scope.lib = "pages/lib.html";
+
+
+
+                  });
---
+++ js/main.js
@@ -0,0 +1,175 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// $('body').toggleClass('fh5co-offcanvas');
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});	
+
+	}
+	
+//kenjin  --->>
+	
+		$("#menu").click(function(e) {
+        e.preventDefault();
+        $(".navbar-collapse").toggleClass("active");
+    });
+	
+	
+
+ $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+   
+	
+	
+	
+
+
+	$(function() {
+        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+	//kenjin  <<---
+	
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+
+	// Document on load.
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		
+
+	});
+
+
+}());
